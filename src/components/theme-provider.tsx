513f564b5c082f087a877fdf6cd720960d56e518
Abhimanyu Singh <122745699+Abhi-boostin@users.noreply.github.com>
2025-02-23 15:25:30 +0530

    fix vercel errors

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,11 +1,33 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider, type ThemeProviderProps as NextThemeProviderProps } from "next-themes"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
 
-export function ThemeProvider({ 
+type Theme = "light" | "dark" | "system"
+type Attribute = "class" | "data-theme"
+
+interface Props {
+  children: React.ReactNode
+  defaultTheme?: Theme
+  enableSystem?: boolean
+  attribute?: Attribute
+}
+
+export function ThemeProvider({
   children,
+  defaultTheme = "system",
+  enableSystem = true,
+  attribute = "class",
   ...props
-}: NextThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}: Props) {
+  return (
+    <NextThemesProvider
+      attribute={attribute}
+      defaultTheme={defaultTheme}
+      enableSystem={enableSystem}
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  )
 } 
